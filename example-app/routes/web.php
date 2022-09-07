1ba5a3fe6c496dc0ecd8f97c7297c5e31aec3d15
Hojiakbar <hatabayev17@gmail.com>
2022-09-07 21:26:58 +0500

    route yozildi

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\GymController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[GymController::class,'index'])->name('index');
+Route::get('/about',[GymController::class,'about'])->name('about');
+Route::get('/featur',[GymController::class,'featur'])->name('featur');
+Route::get('/classes',[GymController::class,'classes']);
+Route::get('/grid',[GymController::class,'grid']);
+Route::get('/detail',[GymController::class,'detail']);
+Route::get('/contact',[GymController::class,'contact']);
